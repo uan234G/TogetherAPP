8c7bfce07871748191e67966341d40f939058973
Rolando Lopantzi <rlopantzi@gmail.com>
2020-04-18 17:44:07 -0700

    init commit

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -33,26 +33,3 @@ function dashNav(){
     menuIcon.classList.toggle('animate')
     mobileDashNav.classList.toggle('show');
 }
-
-
-window.onload = function () {
-    getCovidStats();
-}
-
-function getCovidStats(){
-    fetch('https://coronavirus-tracker-api.herokuapp.com/v2/locations/225')
-    .then(function (resp) { return resp.json() })
-    .then(function (data) {
-        let update = data.location.last_updated;
-        let confirmedCases = data.location.latest.confirmed;
-        let deaths = data.location.latest.deaths;
-        document.getElementById('update').innerHTML = update.substr(0, 10);
-        document.getElementById('cases').innerHTML = confirmedCases.toLocaleString('en');
-        document.getElementById('deaths').innerHTML = deaths.toLocaleString('en');
-        document.getElementById('percent').innerHTML = ((Number(deaths) / Number(confirmedCases)) * 100).toLocaleString("en", { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + "%";
-    })
-    .catch(function () {
-        console.log("error");
-    })
-    setTimeout(getCovidStats, 86400000) // update every 24 hours
-}
